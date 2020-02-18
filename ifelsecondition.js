79161ac50438ffdf168020f72fe4641d7cd2ecbf
chirag patel <chiragbpatel2000@gmail.com>
2020-02-18 16:35:34 +0000

    ifElse statement

---
+++ ifelsecondition.js
@@ -0,0 +1,10 @@
+// let person_age =30;
+// if(person_age>=18){
+//     console.log("He is eligible for insurance")
+// }
+// console.log("program exited");
+let person_age =17;
+if(person_age>=18){
+    console.log("He is eligible for insurance")
+}
+console.log("program exited");
