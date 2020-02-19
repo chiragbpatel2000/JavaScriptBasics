aeee2c03a2c5d3abae9308759101b063821e3186
chirag patel <chiragbpatel2000@gmail.com>
2020-02-19 09:34:39 +0000

    switch case and iflese condition

--- ifelsecondition.js
+++ ifelsecondition.js
@@ -3,8 +3,56 @@
 //     console.log("He is eligible for insurance")
 // }
 // console.log("program exited");
-let person_age =17;
-if(person_age>=18){
-    console.log("He is eligible for insurance")
+// let person_age =17;
+// if(person_age>=18){
+//     console.log("He is eligible for insurance")
+// }
+// console.log("program exited");
+
+// let a = 100, b = 1=20,c=30;
+
+// if(a>b && a>c){
+//     console.log("a is largest number");
+// }
+// else if(b>a && b>c){
+//     console.log("b is largest number");
+// }
+// else
+// {
+//     console.log("c is largest number");
+// }
+
+let weekNo = 8;
+
+if(weekNo==1)
+{
+    console.log("sunday");
 }
-console.log("program exited");
+else if(weekNo==2)
+{
+    console.log("monday"); 
+}
+else if(weekNo==3)
+{
+    console.log("tuesday"); 
+}
+else if(weekNo==4)
+{
+    console.log("wednesday"); 
+}
+else if(weekNo==5)
+{
+    console.log("thursday"); 
+}
+else if(weekNo==6)
+{
+    console.log("friday"); 
+}
+else if(weekNo==7)
+{
+    console.log("saturday"); 
+}
+else
+{
+    console.log("invalid weekno");
+}
